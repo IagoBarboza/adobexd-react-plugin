078df49b59caaf987718447db4806495c4c5bb84
Iago Barboza <iagobarboza@gmail.com>
2021-07-05 19:16:40 -0300

    - Converts to a functional component

--- src/HelloForm.jsx
+++ src/HelloForm.jsx
@@ -1,63 +1,59 @@
-// [1]
-const React = require("react");
-// [2]
-const { Text, Color } = require("scenegraph");
+const React = require('react')
+const { useState } = require('react')
+const { Text, Color } = require('scenegraph')
 
-// [3]
-class HelloForm extends React.Component {
-  // [4]
-  constructor(props) {
-    super(props);
-    this.state = { name: "" }; // [5]
+const HelloForm = ({ selection, dialog }) => {
 
-    // [6]
-    this.onInputChange = e => {
-      this.setState({ name: e.target.value });
-    };
+  const [name, setName] = useState('')
 
-    // [7]
-    this.onDoneClick = e => {
-      // [8]
-      const selection = this.props.selection;
-      // [9]
-      const newText = new Text();
-      newText.text = this.state.name;
-      // [10]
-      newText.styleRanges = [
-        {
-          length: newText.text.length,
-          fill: new Color("#00F"),
-          fontSize: 50
-        }
-      ];
-
-      // [11]
-      selection.insertionParent.addChild(newText);
-      // [12]
-      newText.moveInParentCoordinates(100, 100);
-      // [13]
-      props.dialog.close();
-    };
+  const onInputChange = (e) => {
+    setName(e.target.value)
+    console.log('onInputChanged()')
   }
 
-  // [14]
-  render() {
-    return (
-      <form style={{ width: 300 }} onSubmit={this.onDoneClick}>
-        <h1>React with JSX Components</h1>
-        <label>
-          <span>What is your name?</span>
-          <input onChange={this.onInputChange} />
-        </label>
-        <p>{`Hello ${this.state.name}`}</p>
-        <footer>
-          <button type="submit" uxp-variant="cta">
-            Done
-          </button>
-        </footer>
-      </form>
-    );
+  const onDoneClick = () => {
+    const newText = new Text()
+
+    newText.text = name
+    
+    newText.styleRanges = [
+      {
+        length: newText.text.length,
+        fill: new Color('red'),
+        fontSize: 50
+      }
+    ]
+
+    selection.insertionParent.addChild(newText)
+
+    newText.moveInParentCoordinates(100, 100)
+
+    dialog.close()
   }
+
+  return (
+    <section
+      style={{ width: 300 }}
+    >
+      <h1>React Plugin</h1>
+
+      <label>
+        <span>What is your name?</span>
+        <input onChange={onInputChange} />
+      </label>
+
+      <p>{`Hello ${name}`}</p>
+
+      <footer>
+        <button
+          uxp-variant="cta"
+          onClick={onDoneClick}
+        >
+          Done
+        </button>
+      </footer>
+    </section>
+  )
 }
 
-module.exports = HelloForm;
+module.exports = HelloForm
